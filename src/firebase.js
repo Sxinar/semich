4751d703855d702ecc007fa5c4ca01b65ceeab7d
Sxi <130384567+Sxinar@users.noreply.github.com>
2025-02-04 23:15:52 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const dotenv = require("dotenv");
-dotenv.config();
-
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAvV6ssnLI5sKW3VdmLPyar9JYoT3k7ecw",
+  authDomain: "chatik-843ff.firebaseapp.com",
+  projectId: "chatik-843ff",
+  storageBucket: "chatik-843ff.firebasestorage.app",
+  messagingSenderId: "217834909904",
+  appId: "1:217834909904:web:db2b5527f05e51fe3c6ed5"
 };
 
 const app = initializeApp(firebaseConfig);
