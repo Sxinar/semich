905b94315cd1b06de611329b108323e0d2c1ebbe
Sxi <130384567+Sxinar@users.noreply.github.com>
2025-02-04 22:46:52 +0300

    Update and rename envfirebase.js to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -12,6 +12,9 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+// Firebase konfigürasyonu yazdırarak kontrol edelim
+console.log("Firebase Config: ", firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
