e3972dc0a01d4af9de909dddad169d75ad5bf5b1
Sxi <130384567+Sxinar@users.noreply.github.com>
2025-02-04 23:09:20 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAvV6ssnLI5sKW3VdmLPyar9JYoT3k7ecw",
-  authDomain: "chatik-843ff.firebaseapp.com",
-  projectId: "chatik-843ff",
-  storageBucket: "chatik-843ff.firebasestorage.app",
-  messagingSenderId: "217834909904",
-  appId: "1:217834909904:web:db2b5527f05e51fe3c6ed5"
-};
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
