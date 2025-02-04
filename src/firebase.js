e3a8a79790955537596643a76bf31d7e16c6659e
Sxi <130384567+Sxinar@users.noreply.github.com>
2025-02-04 23:05:12 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-
-
-// Firebase konfigürasyonu yazdırarak kontrol edelim
-console.log('Firebase yapılandırması:', firebaseConfig);
-console.log('Firebase app durumu:', app);
+REACT_APP_FIREBASE_API_KEY=AIzaSyAvV6ssnLI5sKW3VdmLPyar9JYoT3k7ecw
+REACT_APP_FIREBASE_AUTH_DOMAIN=chatik-843ff.firebaseapp.com
+REACT_APP_FIREBASE_PROJECT_ID=chatik-843ff
+REACT_APP_FIREBASE_STORAGE_BUCKET=chatik-843ff.firebasestorage.app
+REACT_APP_FIREBASE_MESSAGING_SENDER_ID=217834909904
+REACT_APP_FIREBASE_APP_ID=1:217834909904:web:db2b5527f05e51fe3c6ed5
 
 
 const app = initializeApp(firebaseConfig);
