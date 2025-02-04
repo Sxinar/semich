e91b7e9d5533fba87a3f27001be9d77932ab156e
Sxi <130384567+Sxinar@users.noreply.github.com>
2025-02-04 23:07:11 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-REACT_APP_FIREBASE_API_KEY=AIzaSyAvV6ssnLI5sKW3VdmLPyar9JYoT3k7ecw
-REACT_APP_FIREBASE_AUTH_DOMAIN=chatik-843ff.firebaseapp.com
-REACT_APP_FIREBASE_PROJECT_ID=chatik-843ff
-REACT_APP_FIREBASE_STORAGE_BUCKET=chatik-843ff.firebasestorage.app
-REACT_APP_FIREBASE_MESSAGING_SENDER_ID=217834909904
-REACT_APP_FIREBASE_APP_ID=1:217834909904:web:db2b5527f05e51fe3c6ed5
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAvV6ssnLI5sKW3VdmLPyar9JYoT3k7ecw",
+  authDomain: "chatik-843ff.firebaseapp.com",
+  projectId: "chatik-843ff",
+  storageBucket: "chatik-843ff.firebasestorage.app",
+  messagingSenderId: "217834909904",
+  appId: "1:217834909904:web:db2b5527f05e51fe3c6ed5"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
